7c054780526da269f09e5a1ecedcd8a0af59837c
+ <+>
2023-01-31 10:35:20 -0500

    adds JS

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  console.log(customerName);
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = "maybe bob"
+    console.log(bestCustomer)
+}
+
+
+const leastFavoriteCustomer = 'asdfsdfasfasdf'
+
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'asdfasdfadsfsdfasdfasdfasd'
+}
